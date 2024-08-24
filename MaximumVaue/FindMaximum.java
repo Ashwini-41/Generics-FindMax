26681e3736930bdfac79f16e0e9bec9ec4513ba7
Ashwini Garad <ashwinigarad1041@gmail.com>
2024-08-24 13:15:03 +0530

    Added : Function to find maximum float value

--- MaximumVaue/FindMaximum.java
+++ MaximumVaue/FindMaximum.java
@@ -13,11 +13,31 @@ public class FindMaximum {
 		}
 	  return max;
 	}
+	
+	public static Float maximumNumber(Float af1, Float bf2, Float cf3) {
+	    Float max = af1;
+		
+		if(bf2.compareTo(max) > 0) {
+			max = bf2;
+		}
+		if(cf3.compareTo(max) > 0) {
+			max = cf3;
+		}
+	  return max;
+	
+	}
 
 	public static void main(String[] args) {
 		Integer a = 30 , b = 225, c = 500;
-		Integer ans = maximumNumber(a,b,c);
-		System.out.println("Maximum number is : " +ans);
+		Integer ans1 = maximumNumber(a,b,c);
+		System.out.println("Maximum Integer value is : " +ans1);
+		
+		Float af1 = 30.5f, bf2 = 45.5f, cf3 = 20.5f;
+		Float ans2 = maximumNumber(af1,bf2,cf3);
+		System.out.println("Maximum Float value is : " +ans2);
+		
 	}
+
+	
 
 }
