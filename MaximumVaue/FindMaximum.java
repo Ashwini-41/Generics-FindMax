55a94dd7ac58dda70f9a0382adfa840444e7607b
Ashwini Garad <ashwinigarad1041@gmail.com>
2024-08-24 12:52:25 +0530

    Added : Function to find max integer value

--- MaximumVaue/FindMaximum.java
+++ MaximumVaue/FindMaximum.java
@@ -4,18 +4,24 @@ public class FindMaximum {
 	
 	public static Integer maximumNumber(Integer a , Integer b , Integer c) {
 		Integer max = a;
-		if(a>b && a > c) {
-			max =a;
-		}else if(b > a && b > c) {
+//		if(a>b && a > c) {
+//			max =a;
+//		}else if(b > a && b > c) {
+//			max = b;
+//		}else {
+//			max = c;
+//		}
+//		
+		if(b.compareTo(max) > 0) {
 			max = b;
-		}else {
+		}else if(c.compareTo(max) > 0) {
 			max = c;
 		}
 	  return max;
 	}
 
 	public static void main(String[] args) {
-		Integer a = 10 , b = 50, c = 12;
+		Integer a = 10 , b = 50, c = 112;
 		Integer ans = maximumNumber(a,b,c);
 		System.out.println("Maximum number is : " +ans);
 	}
